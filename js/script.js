215bd17cafcb634536dea3e9ee8d8eb7d857cc84
bam10112002 <bam10112002@gamil.com>
2022-09-11 20:03:31 +0300

    add files

---
+++ js/script.js
@@ -0,0 +1,441 @@
+
+
+$('.date-row-in-button').on('click', function(event) {
+	
+	$('.date-menu-ferst').toggleClass('date-menu-ferst_active');
+});	
+
+$('.month1').on('click', function(event) {
+	$('.date-row-in-month').html('January');
+	});
+$('.month2').on('click', function(event) {
+	$('.date-row-in-month').html('February');
+	});
+$('.month3').on('click', function(event) {
+	$('.date-row-in-month').html('March');
+	});
+$('.month4').on('click', function(event) {
+	$('.date-row-in-month').html('April');
+	});
+$('.month5').on('click', function(event) {
+	$('.date-row-in-month').html('May');
+	});
+$('.month6').on('click', function(event) {
+	$('.date-row-in-month').html('June');
+	});
+$('.month7').on('click', function(event) {
+	$('.date-row-in-month').html('July');
+	});
+$('.month8').on('click', function(event) {
+	$('.date-row-in-month').html('August');
+	});
+$('.month9').on('click', function(event) {
+	$('.date-row-in-month').html('September');
+	});
+$('.month10').on('click', function(event) {
+	$('.date-row-in-month').html('October');
+	});
+$('.month11').on('click', function(event) {
+	$('.date-row-in-month').html('November');
+	});
+$('.month12').on('click', function(event) {
+	$('.date-row-in-month').html('December');
+	});
+
+
+/*	Меняем день*/
+
+$('.day1').on('click', function(event) {
+	$('.date-row-in-day').html('1');
+	});
+$('.day2').on('click', function(event) {
+	$('.date-row-in-day').html('2');
+	});
+$('.day3').on('click', function(event) {
+	$('.date-row-in-day').html('3');
+	});
+$('.day4').on('click', function(event) {
+	$('.date-row-in-day').html('4');
+	});
+$('.day5').on('click', function(event) {
+	$('.date-row-in-day').html('5');
+	});
+$('.day6').on('click', function(event) {
+	$('.date-row-in-day').html('6');
+	});
+$('.day7').on('click', function(event) {
+	$('.date-row-in-day').html('7');
+	});
+$('.day8').on('click', function(event) {
+	$('.date-row-in-day').html('8');
+	});
+$('.day9').on('click', function(event) {
+	$('.date-row-in-day').html('9');
+	});
+$('.day10').on('click', function(event) {
+	$('.date-row-in-day').html('10');
+	});
+$('.day11').on('click', function(event) {
+	$('.date-row-in-day').html('11');
+	});
+$('.day12').on('click', function(event) {
+	$('.date-row-in-day').html('12');
+	});
+$('.day13').on('click', function(event) {
+	$('.date-row-in-day').html('13');
+	});
+$('.day14').on('click', function(event) {
+	$('.date-row-in-day').html('14');
+	});
+$('.day15').on('click', function(event) {
+	$('.date-row-in-day').html('15');
+	});
+$('.day16').on('click', function(event) {
+	$('.date-row-in-day').html('16');
+	});
+$('.day17').on('click', function(event) {
+	$('.date-row-in-day').html('17');
+	});
+$('.day18').on('click', function(event) {
+	$('.date-row-in-day').html('18');
+	});
+$('.day19').on('click', function(event) {
+	$('.date-row-in-day').html('19');
+	});
+$('.day20').on('click', function(event) {
+	$('.date-row-in-day').html('20');
+	});
+$('.day21').on('click', function(event) {
+	$('.date-row-in-day').html('21');
+	});
+$('.day22').on('click', function(event) {
+	$('.date-row-in-day').html('22');
+	});
+$('.day23').on('click', function(event) {
+	$('.date-row-in-day').html('23');
+	});
+$('.day24').on('click', function(event) {
+	$('.date-row-in-day').html('24');
+	});
+$('.day25').on('click', function(event) {
+	$('.date-row-in-day').html('25');
+	});
+$('.day26').on('click', function(event) {
+	$('.date-row-in-day').html('26');
+	});
+$('.day27').on('click', function(event) {
+	$('.date-row-in-day').html('27');
+	});
+$('.day28').on('click', function(event) {
+	$('.date-row-in-day').html('28');
+	});
+$('.day29').on('click', function(event) {
+	$('.date-row-in-day').html('29');
+	});
+$('.day30').on('click', function(event) {
+	$('.date-row-in-day').html('30');
+	});
+
+
+
+
+
+$('.date-row-out-button').on('click', function(event) {
+	event.preventDefault();
+	$('.date-menu-two').toggleClass('date-menu-two_active');
+});
+
+
+
+
+$('.mmonth1').on('click', function(event) {
+	$('.date-row-out-month').html('January');
+	});
+$('.mmonth2').on('click', function(event) {
+	$('.date-row-out-month').html('February');
+	});
+$('.mmonth3').on('click', function(event) {
+	$('.date-row-out-month').html('March');
+	});
+$('.mmonth4').on('click', function(event) {
+	$('.date-row-out-month').html('April');
+	});
+$('.mmonth5').on('click', function(event) {
+	$('.date-row-out-month').html('May');
+	});
+$('.mmonth6').on('click', function(event) {
+	$('.date-row-out-month').html('June');
+	});
+$('.mmonth7').on('click', function(event) {
+	$('.date-row-out-month').html('July');
+	});
+$('.mmonth8').on('click', function(event) {
+	$('.date-row-out-month').html('August');
+	});
+$('.mmonth9').on('click', function(event) {
+	$('.date-row-out-month').html('September');
+	});
+$('.mmonth10').on('click', function(event) {
+	$('.date-row-out-month').html('October');
+	});
+$('.mmonth11').on('click', function(event) {
+	$('.date-row-out-month').html('November');
+	});
+$('.mmonth12').on('click', function(event) {
+	$('.date-row-out-month').html('December');
+	});
+
+
+/*	Меняем день*/
+
+$('.dday1').on('click', function(event) {
+	$('.date-row-out-day').html('1');
+	});
+$('.dday2').on('click', function(event) {
+	$('.date-row-out-day').html('2');
+	});
+$('.dday3').on('click', function(event) {
+	$('.date-row-out-day').html('3');
+	});
+$('.dday4').on('click', function(event) {
+	$('.date-row-out-day').html('4');
+	});
+$('.dday5').on('click', function(event) {
+	$('.date-row-out-day').html('5');
+	});
+$('.dday6').on('click', function(event) {
+	$('.date-row-out-day').html('6');
+	});
+$('.dday7').on('click', function(event) {
+	$('.date-row-out-day').html('7');
+	});
+$('.dday8').on('click', function(event) {
+	$('.date-row-out-day').html('8');
+	});
+$('.dday9').on('click', function(event) {
+	$('.date-row-out-day').html('9');
+	});
+$('.dday10').on('click', function(event) {
+	$('.date-row-out-day').html('10');
+	});
+$('.dday11').on('click', function(event) {
+	$('.date-row-out-day').html('11');
+	});
+$('.dday12').on('click', function(event) {
+	$('.date-row-out-day').html('12');
+	});
+$('.dday13').on('click', function(event) {
+	$('.date-row-out-day').html('13');
+	});
+$('.dday14').on('click', function(event) {
+	$('.date-row-out-day').html('14');
+	});
+$('.dday15').on('click', function(event) {
+	$('.date-row-out-day').html('15');
+	});
+$('.dday16').on('click', function(event) {
+	$('.date-row-out-day').html('16');
+	});
+$('.dday17').on('click', function(event) {
+	$('.date-row-out-day').html('17');
+	});
+$('.dday18').on('click', function(event) {
+	$('.date-row-out-day').html('18');
+	});
+$('.dday19').on('click', function(event) {
+	$('.date-row-out-day').html('19');
+	});
+$('.dday20').on('click', function(event) {
+	$('.date-row-out-day').html('20');
+	});
+$('.dday21').on('click', function(event) {
+	$('.date-row-out-day').html('21');
+	});
+$('.dday22').on('click', function(event) {
+	$('.date-row-out-day').html('22');
+	});
+$('.dday23').on('click', function(event) {
+	$('.date-row-out-day').html('23');
+	});
+$('.dday24').on('click', function(event) {
+	$('.date-row-out-day').html('24');
+	});
+$('.dday25').on('click', function(event) {
+	$('.date-row-out-day').html('25');
+	});
+$('.dday26').on('click', function(event) {
+	$('.date-row-out-day').html('26');
+	});
+$('.dday27').on('click', function(event) {
+	$('.date-row-out-day').html('27');
+	});
+$('.dday28').on('click', function(event) {
+	$('.date-row-out-day').html('28');
+	});
+$('.dday29').on('click', function(event) {
+	$('.date-row-out-day').html('29');
+	});
+$('.dday30').on('click', function(event) {
+	$('.date-row-out-day').html('30');
+	});
+
+var total = 1;
+
+
+$('.number-min').on('click', function(event) {
+	event.preventDefault();
+	total=total-1;
+	$('.number-span').html(total);
+});
+
+$('.number-plus').on('click', function(event) {
+	event.preventDefault();
+	total=total+1;
+	$('.number-span').html(total);
+});
+
+
+
+
+$('.awards').slick({
+	infinite: true,
+	slidesToShow: 2,
+	infinite: true,
+  	dots: true,
+  	slidesToScroll: 2,
+  	responsive: [
+    {
+      	breakpoint: 810,
+      	settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }
+  ]
+});
+
+$('.activity').slick({
+	infinite: true,
+	slidesToShow: 1,
+	infinite: true,
+  	dots: true,
+  	prevArrow: '<button class="prev"><img src="../img/Activity/丳〰 7 꾿菠2.png" height="100%" alt="" /></button>',
+  	nextArrow: '<button class="next"><img src="../img/Activity/丳〰 7 꾿菀.png" height="100%" alt="" /></button>',
+  	slidesToScroll: 1
+  
+});
+	
+$('.news').slick({
+	infinite: true,
+	slidesToShow: 3,
+	infinite: true,
+  	dots: true,
+  	prevArrow: '<button class="prev_arrow"><img src="../img/Activity/丳〰 7 꾿菠2.png" height="100%" alt="" /></button>',
+  	nextArrow: '<button class="next_arrow"><img src="../img/Activity/丳〰 7 꾿菀.png" height="100%" alt="" /></button>',
+  	slidesToScroll: 1,
+  	responsive: [
+    {
+      	breakpoint: 1146,
+      	settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+     {
+      	breakpoint: 750,
+      	settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }
+  ]
+});	
+var arrow = false;
+
+$('.arrow').on('click', function(event) {
+	if($(window).width() <= 500) {
+		 $('.menu').css({
+	        	'width':'100%'
+	   		 });
+ 		 $('.menu-logo').css({
+	        	'margin-top':'50px'
+	   		 });
+ 		 if (arrow == false){
+			arrow = true;
+			 $('.telephon-country').css({
+	        	'position' : 'absolute',
+	        	'right' : '35%'
+	   		 });
+			  
+			 $('.menu').css({
+	        	'transform': 'translateX(0px)',
+	        	'display': 'block'
+		    });
+		} else{
+			arrow=false;
+			 $('.telephon-country').css({
+	        	'position' : 'static',
+	        	'right' : '0'
+		    });
+			 $('.menu').css({
+	        	'transform': 'translateX(300px)',
+	        	'display': 'none'
+	       	});
+		}
+ 	}else if($(window).width() <= 700) {
+ 		 $('.menu-logo').css({
+	        	'margin-top':'50px'
+	   		 });
+ 		 if (arrow == false){
+			arrow = true;
+			 $('.telephon-country').css({
+	        	'position' : 'absolute',
+	        	'right' : '80px'
+	   		 });
+			  
+			 $('.menu').css({
+	        	'transform': 'translateX(0px)',
+	        	'display': 'block'
+		    });
+		} else{
+			arrow=false;
+			 $('.telephon-country').css({
+	        	'position' : 'static',
+	        	'right' : '0'
+		    });
+			 $('.menu').css({
+	        	'transform': 'translateX(300px)',
+	        	'display': 'none'
+	       	});
+		}
+ 	}else{
+		if (arrow == false){
+			arrow = true;
+			 $('.telephon-country').css({
+	        	'position' : 'absolute',
+	        	'right' : '300px'
+	   		 });
+			  
+			 $('.menu').css({
+	        	'transform': 'translateX(0px)',
+	        	'display': 'block'
+		    });
+		} else{
+			arrow=false;
+			 $('.telephon-country').css({
+	        	'position' : 'static',
+	        	'right' : '0'
+		    });
+			 $('.menu').css({
+	        	'transform': 'translateX(300px)',
+	        	'display': 'none'
+	       	});
+		}
+	}
+});
